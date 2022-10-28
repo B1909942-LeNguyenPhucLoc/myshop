98dcea54443e70c7e9c2b0097db26e64e0bfaf93
locb1909942 <locb1909942@student.ctu.edu.vn>
2022-10-28 23:26:20 +0700

    Luu san pham yeu thich len Realtime Database cua Firebase

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -3,7 +3,7 @@ import '../../models/product.dart';
 import 'product_detail_screen.dart';
 import '../cart/cart_manager.dart';
 import 'package:provider/provider.dart';
-
+import 'products_manager.dart';
 class ProductGridTile extends StatelessWidget {
   const ProductGridTile(
     this.product, {
@@ -44,7 +44,7 @@ class ProductGridTile extends StatelessWidget {
             ),
             color: Theme.of(context).colorScheme.secondary,
             onPressed: () {
-              product.isFavorite = !isFavorite;
+              ctx.read<ProductsManager>().toggleFavoriteStatus(product);
             },
           );
         },
